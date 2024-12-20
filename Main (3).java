2bac2f92b8243ff3c9d4cb85673c16081da31fca
PIYUSH SINGH <singhpiyush1403@gmail.com>
2024-12-20 22:08:44 +0530

    Add files via upload

---
+++ Main (3).java
@@ -0,0 +1,34 @@
+class Animal {
+
+    public void sound() {
+        System.out.println("Animal makes a sound");
+    }
+}
+
+class Dog extends Animal {
+    
+    @Override
+    public void sound() {
+        System.out.println("Dog barks");
+    }
+}
+
+class Cat extends Animal {
+   
+    @Override
+    public void sound() {
+        System.out.println("Cat meows");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Animal myAnimal = new Animal();  
+        Animal myDog = new Dog();       
+        Animal myCat = new Cat();        
+
+        myAnimal.sound(); 
+        myDog.sound();   
+        myCat.sound();    
+    }
+}
